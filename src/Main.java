c56fe8f9434b7bb2791f25a865e6029f0f4860f4
Александр <alexakr1993@gmail.com>
2023-03-03 15:12:13 +0400

    begin

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String words = "yourapp the quick brown fox jumps over the lazy dog";
+        System.out.println("Top 10: ");
+        Arrays.stream(words.split(" "))
+                .collect(Collectors.groupingBy(s->s,Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(java.util.Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach((e -> System.out.println(e.getKey() + " - " + e.getValue())));
+
     }
 }
